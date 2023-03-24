8cc6edb3f86efda6dd2eff44dcae0e18df88ceec
储天航 <1193983801@qq.com>
2023-03-24 17:18:40 +0800

    初始化

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useRef, useEffect } from "react";
+import fire1 from "./assets/img1/Normal_20230323095349.jpg";
+import fire1_black from "./assets/img1/Normal_20230323213824.jpg";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+
+    // 绘制矩形
+    context.fillStyle = "#f00";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    // 绘制文本
+    context.fillStyle = "#fff";
+    context.font = "bold 24px Arial";
+    context.fillText("Hello, world!", 50, 90);
+    // 在这里进行canvas渲染逻辑
+  }, [canvasRef.current]);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <canvas ref={canvasRef} width="500" height="500">
+        {/* <img src={fire1} />
+        <img src={fire1_black} /> */}
+      </canvas>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
