3982b28936383a800160d5278c41fcb1b4a8a828
ThomasHang <11939838031@qq.com>
2023-03-28 00:09:47 +0800

    优化

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
  * @Author: ThomasHang 11939838031@qq.com
  * @Date: 2023-03-25 20:04:18
  * @LastEditors: ThomasHang 11939838031@qq.com
- * @LastEditTime: 2023-03-25 23:25:18
+ * @LastEditTime: 2023-03-28 00:08:31
  * @FilePath: /canvas_image/src/App.jsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -53,42 +53,7 @@ function App() {
   //   // 在这里进行canvas渲染逻辑
   // }, [canvasRef.current]);
 
-  //处理图片黑色背景变透明
-  // useEffect(() => {
-  //   const canvas = canvasRef.current;
-  //   const ctx = canvas.getContext('2d');
-  //   const image1 = new Image();
-  //   const image2 = new Image();
-  //   image1.src = fire1;
-  //   image2.src = fire1_black;
-
-  //   Promise.all([loadImage(image1), loadImage(image2)]).then((images) => {
-  //     canvas.width = image1.width;
-  //     canvas.height = image1.height;
-  //     ctx.drawImage(images[1], 0, 0);
-
-  //     const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  //     const data = imageData.data;
-
-  //     // 循环遍历像素数据，将黑色像素点转换为透明
-  //     for (let i = 0; i < data.length; i += 4) {
-  //       if (data[i] === 0 && data[i + 1] === 0 && data[i + 2] === 0) {
-  //         data[i + 3] = 0;
-  //       }
-  //     }
-  //     ctx.putImageData(imageData, 0, 0);
-
-  //     // 将第二张图片叠加到第一张图片上
-  //     ctx.globalCompositeOperation = 'destination-over';
-  //     ctx.drawImage(images[0], 0, 0);
-
-  //     // 将画布内容输出为一张图片
-  //     const outputImage = canvas.toDataURL('image/png');
-  //     console.log(outputImage); // 输
-  //   });
-  // }, []);
-
-  //处理红框图片 背景变透明
+  // 处理图片黑色背景变透明
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
@@ -96,48 +61,33 @@ function App() {
     const image2 = new Image();
     image1.src = fire1;
     image2.src = fire1_black;
+
     Promise.all([loadImage(image1), loadImage(image2)]).then((images) => {
       canvas.width = image1.width;
       canvas.height = image1.height;
       ctx.drawImage(images[1], 0, 0);
 
+      // canvas.addEventListener("mousemove", newHandleMouseMove);
+      // 绘制矩形
+      ctx.fillStyle = 'rgba(0, 0, 0, 0)'; // 设置填充色为透明
+      ctx.fillRect(186, 844.5, 40, 30);
+
       const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
       const data = imageData.data;
 
-      // 找到红色矩形框的边界
-      let left = canvas.width;
-      let top = canvas.height;
-      let right = 0;
-      let bottom = 0;
+      // 循环遍历像素数据，将黑色像素点转换为透明
+      // for (let i = 0; i < data.length; i += 4) {
+      //   if (data[i] === 0 && data[i + 1] === 0 && data[i + 2] === 0) {
+      //     data[i + 3] = 0;
+      //   }
+      // }
+      // ctx.putImageData(imageData, 0, 0);
 
+      // 遍历像素数据，将矩形外的像素颜色值设置为透明
       for (let y = 0; y < canvas.height; y++) {
         for (let x = 0; x < canvas.width; x++) {
-          const i = (y * canvas.width + x) * 4;
-          if (data[i] === 255 && data[i + 1] === 0 && data[i + 2] === 0) {
-            left = Math.min(left, x);
-            top = Math.min(top, y);
-            right = Math.max(right, x);
-            bottom = Math.max(bottom, y);
-          }
-        }
-      }
-
-      // left -= 20;
-      // top -= 20;
-      // right += 20;
-      // bottom += 20;
-
-      // 遍历像素数据，将矩形框边界+20像素点范围外的其他像素颜色值设置为透明
-      for (let y = 0; y < canvas.height; y++) {
-        for (let x = 0; x < canvas.width; x++) {
-          const i = (y * canvas.width + x) * 4;
-          if (
-            x < left ||
-            x > right ||
-            y < top ||
-            y > bottom ||
-            (data[i] === 255 && data[i + 1] === 0 && data[i + 2] === 0)
-          ) {
+          if (x < 156 || x > 280 || y < 830 || y >900) {
+            const i = (y * canvas.width + x) * 4;
             data[i + 3] = 0; // 设置 alpha 值为 0
           }
         }
@@ -145,8 +95,22 @@ function App() {
 
       // 将修改后的像素数据绘制到画布上
       ctx.putImageData(imageData, 0, 0);
+
+      // 将第二张图片叠加到第一张图片上
+      ctx.globalCompositeOperation = 'destination-over';
+      ctx.drawImage(images[0], 0, 0);
+
+      // 将画布内容输出为一张图片
+      const outputImage = canvas.toDataURL('image/png');
+      console.log(outputImage); // 输
     });
+
+    return () => {
+      // 移除鼠标移动事件
+      canvas.removeEventListener("mousemove", newHandleMouseMove);
+    };
   }, []);
+
 
   // 加载图片
   function loadImage(image) {
@@ -187,6 +151,14 @@ function App() {
     // 将修改后的像素数据绘制到画布上
     ctx.putImageData(imageData, x - 10, y - 10);
   }
+
+  const newHandleMouseMove = (event) => {
+    const canvas = canvasRef.current;
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    console.log(`x: ${x}, y: ${y}`);
+  };
 
   return (
     <div className="App">
